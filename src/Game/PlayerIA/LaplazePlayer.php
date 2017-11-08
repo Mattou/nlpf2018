6caaf9b78e870022525fc669ae073a4ad946b52b
Mattou <mathilde.lplz@gmail.com>
2017-11-08 11:41:45 -0800

    first try

--- src/Game/PlayerIA/LaplazePlayer.php
+++ src/Game/PlayerIA/LaplazePlayer.php
@@ -15,10 +15,84 @@ class LaplazePlayer extends Player
     protected $opponentSide;
     protected $result;
     protected $oponent_last_3 = array();
+    protected $third_last = 0;
+    protected $sec_last = 0;
+    protected $last = 0;
+    protected $size = 3; //width of the array
 
+
+    function __construct() {
+        for ($i = 0; $i <= $size * $size; $i++) {
+            $this->oponent_last_3[$i] = 0;
+        }
+    }
+// Rock: 1
+//Paper: 2
+// Scissors: 3
 
     public function getChoice()
     {
+
+        if ($this->result->getNbRound() == 3) {
+            $tmpChoices = $this->result->getChoicesFor($this->opponentSide);
+            $this->third_last = $tmpChoices[0];
+            $this->sec_last = $tmpChoices[1];
+            $this->last = $tmpChoices[2];
+        } else if ($this->result->getNbRound() > 3) {
+            $this->third_last = $sec_last;
+            $this->sec_last = $last;
+            $this->last = $this->result->getLastChoiceFor($this->opponentSide);
+        } else {
+            return parent::rockChoice();
+        }
+
+        $index = 0; //index to increment
+        switch ($this->third_last) {
+            case 'scissors':
+                $index = $this->size * $this->size;
+                break;
+            case 'paper':
+            $index = $this->size * $this->size * 2;
+                break;
+        }
+        switch ($this->sec_last) {
+            case 'scissors':
+                $index = $index + $this->size;
+                break;
+            case 'paper':
+            $index = $index + ($this->size * 2);
+                break;
+        }
+        $res = $index; //where we will get the proba
+        switch ($sec_last) {
+            case 'scissors':
+                $index = $index + 1;
+                break;
+            case 'paper':
+            $index = $index + 2;
+                break;
+        }
+        $this->opponent_last_3[$index] = $this->opponent_last_3[$index] + 1;
+        $max_val = $res;
+        if ($this->opponent_last_3[$res] < $this->opponent_last_3[$res + 1]) {
+            $max_val = $res + 1;
+        }
+        if ($this->opponent_last_3[$max_val] < $this->opponent_last_3[$res + 2]) {
+            $max_val = $res + 2;
+        }
+
+        switch ($max_val % 3) {
+            case 0:
+            return parent::paperChoice();
+                break;
+            case 1:
+            return parent::rockChoice();
+                break;
+            default:
+            return parent::scissorsChoice();
+            break;
+        }
+
         // -------------------------------------    -----------------------------------------------------
         // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
         // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
@@ -42,9 +116,5 @@ class LaplazePlayer extends Player
         // -------------------------------------    -----------------------------------------------------
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
-
-        $choice = parent::rockChoice();
-
-        return $choice;
     }
 };
